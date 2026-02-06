d8cf968841cf348a1b023ea33af168706769bbfa
Muhammad ayub <muhammadayubmrk@gmail.com>
2026-02-06 21:56:23 +0500

    layout disabled temporarily

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -19,23 +19,23 @@ const font = Orbitron({
 export default function Home() {
   const {isclose,setClose} = useContext(NavContext)
   return (
-    <div className="mx-auto max-w-360 relative">
-      <Header isclose={isclose} setClose={setClose} />
-      <Navbar isclose={isclose} setClose={setClose} />
-      <Hero/>
-      <Offer/>
-      <Categories/>
-      <OurProducts/>
-      <BestDeal/>
-      <NewRelease/>
-    </div>
-    
-    // <div className="p-3 locker flex items-center justify-center w-[100%] h-[100vh]">
-    //  <div className="">
-    //   <p className={` ${font.className} text-center tracking-widest text-6xl text-[#e6c409] `}>Coming Soon!</p>
-    //   <p className={`${font.className} tracking-widest text-lg text-center mt-5 font-semibold text-[#d6b80c]`}>Our website will be available soon. Stay tuned!</p>
-      
-    //  </div>
+    // <div className="mx-auto max-w-360 relative">
+    //   <Header isclose={isclose} setClose={setClose} />
+    //   <Navbar isclose={isclose} setClose={setClose} />
+    //   <Hero/>
+    //   <Offer/>
+    //   <Categories/>
+    //   <OurProducts/>
+    //   <BestDeal/>
+    //   <NewRelease/>
     // </div>
+    
+    <div className="p-3 locker flex items-center justify-center w-[100%] h-[100vh]">
+     <div className="">
+      <p className={` ${font.className} text-center tracking-widest text-6xl text-[#e6c409] `}>Coming Soon!</p>
+      <p className={`${font.className} tracking-widest text-lg text-center mt-5 font-semibold text-[#d6b80c]`}>Our website will be available soon. Stay tuned!</p>
+      
+     </div>
+    </div>
   );
 }
