6341f991e75ec9ac6d8573a86f4cd6c350580ee4
Muhammad ayub <muhammadayubmrk@gmail.com>
2026-03-13 00:05:29 +0500

    5 deploymen issue fixing

--- src/app/HaqLedLightsSpareParts/page.jsx
+++ src/app/HaqLedLightsSpareParts/page.jsx
@@ -8,22 +8,22 @@ import MainTitle from '../components/mainTitle'
 import ItemCard from '../components/itemcard'
 import Link from 'next/link'
 const HaqLedLightsSpareParts = () => {
-    const {isclose,setClose,product} = useContext(NavContext)
+    const {isclose,setClose,products} = useContext(NavContext)
   return (
     <div>
    
       <MainTitle title={'Haq Spare Parts'} />
        <div className='grid grid-cols-[repeat(auto-fit,minmax(220px,1fr))] gap-10  mx-[5%]'>
          {
-          product.map((item, index) => {
+          products.map((item, index) => {
             if (item.category == "Spare_Parts") {
-              return <Link key={item.id} href={`/Stage/${item.id}`}>
+              return <Link key={item._id} href={`/Stage/${item._id}`}>
                   <div className="" key={index}>
                       <ItemCard
-                      id={item.id}
+                      id={item._id}
                       src={item.images[0]}
-                      title={item.title}
-                      discount={item.discountPercentage}
+                      title={item.name}
+                      discount={item.discount}
                       price={item.price}
                       originalPrice={item.originalPrice}
                     />
