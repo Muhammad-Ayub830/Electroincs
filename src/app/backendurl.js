6e8908d27719a034df9e69da7271eb444d1fb1ce
Muhammad ayub <muhammadayubmrk@gmail.com>
2026-03-13 17:57:18 +0500

    unsaved url saved

--- src/app/backendurl.js
+++ src/app/backendurl.js
@@ -1,4 +1,4 @@
 
-     // let backendUrl   = 'https://electronics-website-backend.vercel.app/'
-     let backendUrl   = 'http://localhost:4000/'
+     let backendUrl   = 'https://electronics-website-backend.vercel.app/'
+     // let backendUrl   = 'http://localhost:4000/'
      module.exports = backendUrl;
