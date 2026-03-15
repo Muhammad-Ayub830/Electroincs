988e87d476f778474f4143ff5d73185021d50d7e
Muhammad ayub <muhammadayubmrk@gmail.com>
2026-03-15 21:22:09 +0500

    sub domain added

--- src/app/backendurl.js
+++ src/app/backendurl.js
@@ -1,4 +1,5 @@
 
-     let backendUrl   = 'https://electronics-website-backend.vercel.app/'
+     let backendUrl   = 'https://api.haq1.com/'
+     // let backendUrl   = 'https://electronics-website-backend.vercel.app/'
      // let backendUrl   = 'http://localhost:4000/'
      module.exports = backendUrl;
