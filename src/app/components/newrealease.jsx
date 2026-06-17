cb566cc09f621143e6f00b446fd834357ae518ec
Muhammad ayub <muhammadayubmrk@gmail.com>
2026-06-17 13:44:36 +0500

    the issue of category

--- src/app/components/newrealease.jsx
+++ src/app/components/newrealease.jsx
@@ -4,20 +4,22 @@ import ItemCard from './itemcard'
 import { NavContext } from './context/context'
 
 const NewRelease = () => {
-  const { products } = useContext(NavContext)
+  const { products, category } = useContext(NavContext)
 
   return (
     <div className="my-20">
       <MainTitle title={'New Release'} />
 
-      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 px-[5%] gap-7 w-full'>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 px-[5%] gap-7 w-full">
 
         {products?.length > 0 ? (
           products
-  .filter(item => category === 'All' || item.category === category)
-  .slice(0, 10)
-            .map((item) => {
-              if (item.isNewArrival)
+            .filter(item =>
+              category === 'All' || item.category === category
+            )
+            .slice(0, 10)
+            .map(item => {
+              if (item.isNewArrival) {
                 return (
                   <ItemCard
                     key={item._id}
@@ -30,13 +32,12 @@ const NewRelease = () => {
                     tags={item.tags}
                   />
                 )
+              }
+              return null
             })
         ) : (
           Array.from({ length: 3 }).map((_, index) => (
-            <div
-              key={index}
-              className="animate-pulse  "
-            >
+            <div key={index} className="animate-pulse">
               <div className="h-48 bg-gray-300 rounded"></div>
               <div className="h-4 bg-gray-300 rounded mt-4 w-3/4"></div>
               <div className="h-4 bg-gray-300 rounded mt-2 w-1/2"></div>
